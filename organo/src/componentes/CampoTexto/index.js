f239b23e6a6da01b4a811a820988e0112a569bc1
Deivid Daniel <79475211+dev-br18@users.noreply.github.com>
2022-12-19 15:46:58 -0300

    aulas 1

---
+++ organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,13 @@
+
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return(
+        <div className='campo-texto'>
+            <label>Nome</label>
+            <input placeholder='Digite Seu Nome'/>
+        </div>
+    )
+}
+
+export default CampoTexto
